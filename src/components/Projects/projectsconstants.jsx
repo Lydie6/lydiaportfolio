336cbe0e6ece4e8c078e1b821c75b605ce084401
Lydie6 <ikhleflydia7@gmail.com>
2025-07-10 22:39:59 +0100

    Final version

--- src/components/Projects/projectsconstants.jsx
+++ src/components/Projects/projectsconstants.jsx
@@ -4,21 +4,21 @@ export const projects = [
       id: 1,
       title: "Interior Design Landing Page ",
       description: "Elegant landing page for an interior decoration company, showcasing services, portfolio, and contact ",
-      technologies: ["React", "Node.js", "AWS", "Docker"],
+      technologies: ["React", "TailwindCss"],
       link: "https://decoodar.netlify.app/"
     },
     {
       id: 2,
       title: "Real estate App",
       description: "Front-end interface for a real estate rental platform, designed to help users easily browse, search, and filter property listings. ",
-      technologies: ["Next.js", "Stripe", "MongoDB", "Tailwind"],
+      technologies: ["React", "CSS"],
       link: "https://frontendinterfacerealeestatewebapp.netlify.app/"
     },
     {
       id: 3,
       title: "Restaurant Management System ",
       description: "A distributed client-server application for managing restaurant orders, tables, and kitchen coordination",
-      technologies: ["Python", "TensorFlow", "D3.js", "FastAPI"],
+      technologies: ["Java"],
       link: "https://github.com/Lydie6/RestaurauntMangement"
     }
   ];
