f2337f85e88fbd05ff12a7f8f69cdbc3f1aff33c
Lydie6 <ikhleflydia7@gmail.com>
2025-07-10 16:57:34 +0100

    Final version

--- src/components/LoadingScreen.jsx
+++ src/components/LoadingScreen.jsx
@@ -23,10 +23,10 @@ const LoadingScreen = ({ onComplete }) => {
 
   return (
     <div className="fixed inset-0 z-[9999] bg-black text-gray-100 flex flex-col justify-center items-center">
-      <div className="text-4xl font-mono font-bold">
-        <span className="animate-blink ml-1">{text}|</span>
+      <div className=" text-center text-[30px] sm:text-2xl text-4xl font-mono font-bold">
+        <span className="animate-blink ml-1 ">{text}|</span>
       </div>
-      <div className="w-[200px] h-[3px] bg-gray-800 rounded relative overflow-hidden">
+      <div className=" w-[80vw] mt-4 sm:mt-1 sm:w-[400px] h-[3px] bg-gray-800 rounded relative overflow-hidden">
         <div className="w-[40%] h-full bg-blue-500 shadow-[0_0_15px_#3b82f6] animate-loading-bar"></div>
       </div>
     </div>
